81bad854f4a224904c888955d52950bd4e88971d
Sacha Bron <sacha.bron@copperflake.com>
2017-11-11 16:29:45 +0100

    Init web view

---
+++ config.js
@@ -0,0 +1,6 @@
+export const config = {
+    apiKey: "AIzaSyAZmg1qH9lSntOWl2EuM9b7GBRXOt6WDZ8",
+    authDomain: "fidelizy-b765c.firebaseapp.com",
+    databaseURL: "https://fidelizy-b765c.firebaseio.com/",
+    storageBucket: "gs://fidelizy-b765c.appspot.com",
+};
