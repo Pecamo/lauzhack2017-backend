dd2d934481945b1a532be8da9e5075a0aa491424
Basile Vu <basile.vu@gmail.com>
2017-11-11 22:59:33 +0100

    Vue components

--- app/script/vue-app.js
+++ app/script/vue-app.js
@@ -1,25 +1,28 @@
 let promosRef = firebase.database().ref('/businesses/holycow/FCs/FC1/promos')
 console.log(123, promosRef.toJSON())
 
-// create Vue app
-let app = new Vue({
-	// element to mount to
-	el: '#app',
-	// initial data
-	data: {
+Vue.component("fc-promo", {
+	props: ["name", "points"],
+	template: `
+	<div class="promotion pure-g">
+		<strong class="pure-u-1-4">
+			{{ name }}
+		</strong>
+		<div class="pure-u-3-4">
+			{{ points }}
+			<span class="fa fa-pencil"></span>
+		</div>
+	</div>
+	`
+})
+
+Vue.component("fc-promos-edit", {
+	data: () => ({
 		newPromo: {
 			key: '',
 			value: ''
 		}
-	},
-
-	// firebase binding
-	// https://github.com/vuejs/vuefire
-	firebase: {
-		promos: promosRef
-	},
-	
-	// methods
+	}),
 	methods: {
 		addPromo: function () {
 			promosRef.push(this.newPromo)
@@ -29,5 +32,63 @@ let app = new Vue({
 		removePromo: function (promo) {
 			promosRef.child(promo['.key']).remove()
 		}
-	}
+	},
+	// firebase binding
+	// https://github.com/vuejs/vuefire
+	firebase: {
+		promos: promosRef
+	},
+	template: `
+	<div>
+		<ul is="transition-group">
+			<li v-for="promo in promos" class="promo" :key="promo['.key']">
+				<span>{{promo.key}} - {{promo.value}}</span>
+				<button v-on:click="removePromo(promo)">X</button>
+			</li>
+		</ul>
+		<form id="form" v-on:submit.prevent="addPromo">
+			<input type="text" v-model="newPromo.key" placeholder="Key">
+			<input type="text" v-model="newPromo.value" placeholder="Value">
+			<input type="submit" value="Add promo">
+		</form>
+	</div>
+	`
+})
+
+Vue.component("fc-promotions", {
+	template: `
+	<div>
+		<h3>Promotions</h3>
+		<fc-promos-edit></fc-promos-edit>
+		<fc-promo name="1 free drink" points="2"></fc-promo>
+		<fc-promo name="1 free burger" points="10"></fc-promo>
+	</div>
+	`
+})
+
+Vue.component("fc-card", {
+	props: ["description"],
+	template: 
+	`<div class="fc-card">
+		<h2>FC cow</h2>
+		<div>
+			<h3>
+				Description
+				<span class="fa fa-pencil"></span>
+			</h3>
+			<p>{{ description }}</p>
+		</div>
+		<fc-promotions></fc-promotions>
+	</div>`
+})
+
+// create Vue app
+let app = new Vue({
+	// element to mount to
+	el: '#app',
+	// initial data
+	data: {},
+	
+	// methods
+	methods: {}
 })
